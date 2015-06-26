e77d01521912fd6214f393c21654b7a86b675c59
Elf-mousE <ifmouz@gmail.com>
2015-06-26 09:10:17 +0800

    flux

---
+++ flux-backbone/examples/todos/constants/Constants.js
@@ -0,0 +1,16 @@
+    /**
+     * Constants used by WebApiMixin to disaptch events to handle server calls.
+     */
+
+    "use strict";
+
+    var Constants = {
+
+        request: {
+            ERROR: "REQUEST.ERROR",
+            PENDING: "REQUEST.PENDING"
+        }
+    };
+
+     module.exports = Constants;
+
